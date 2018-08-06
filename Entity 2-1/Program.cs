f3f9d7807b695ebfa87ac85a9ad49d626ed07365
Anton Bialenik <tony.baton@yandex.by>
2018-08-06 16:26:15 +0300

    First commit

---
+++ Entity 2-1/Program.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Entity_2_1
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+        public class UserContext : DbContext
+        {
+            public UserContext() :
+                base("UserDB")
+            { }
+
+            public DbSet<User> Users { get; set; }
+        }
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (UserContext db = new UserContext())
+            {
+                var users = db.Users;
+                foreach (User u in users)
+                {
+                    Console.WriteLine("{0}.{1} - {2}", u.Id, u.Name, u.Age);
+                }
+                Console.Read();
+            }
+        }
+        
+    } 
+}
